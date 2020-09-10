8396949013293151c267d90fbe8d4373d0194309
Subhendu De <subhendu.de@microsoft.com>
2020-09-10 22:06:29 +0530

    Modify assignment commands

--- 01. User and Group Management/Assignment1.sh
+++ 01. User and Group Management/Assignment1.sh
@@ -4,24 +4,31 @@ groupadd guests
 tail etc/group
 
 #Start with creating a new user Jane and add Jane to the members group
-useradd -G members jane
+useradd -m -G members jane
 passwd jane
 
 # Start with creating a new user Tom and add Tom to the members group
-useradd -c "User Tom for assignment" -G members -p 1234 tom
+useradd -m -c "User Tom for assignment" -G members tom
+passwd tom
 
-# Create three folders
+# Create three folders while logging as root
 mkdir linux
 mkdir notes
 mkdir research
 
 # Put proper permissions for the groups and folders
+# linux repository/directory can be acessed by users who are part of members group. The user owner and group owner of linux directory is root and members respectively.
 chgrp members linux
-chgrp members notes
-chmod 454 notes
+# The user owner(root) has all permissions, group members have all permission, others dont have any permission 
+chmod 770 linux
 
-chown jane research
-chmod 705 research
+# notes repository/directory can be acessed by users who are part of members group. The user owner and group owner of notes directory is root and members respectively.
+chgrp members notes
+chmod 775 notes
+
+# research repository/directory cannot be acessed by users except jane. The user owner and group owner of research directory is jane and root respectively.
+chown jane:root research
+chmod 701 research
 
 
 
