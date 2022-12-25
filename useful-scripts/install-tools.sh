f1b764361f1d8a08b133dab2c72d7e87948cac67
Subhendu De <subhendude@gmail.com>
2022-12-25 18:55:08 +0000

    add few more tools information

--- useful-scripts/install-tools.sh
+++ useful-scripts/install-tools.sh
@@ -1,8 +1,13 @@
 sudo apt update
 sudo apt upgrade
 # Install the network tools
-sudo apt install net-tools git
+# net-tools - contains tools used in regular work
+# nmap - network mapping tool
+sudo apt install net-tools nmap
 # Install the git
 sudo apt install git
 # Install vim, vim-tutor and vim-nox
 sudo apt install vim vim-nox
+# Install other tools
+# vlock - lock the virtual console
+sudo apt install vlock
