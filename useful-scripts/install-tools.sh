68fc9bc7b0b4abac7827b5ceedd755f18b3e74c8
Subhendu De <subhendude@gmail.com>
2022-12-25 14:57:38 +0000

    add comments and vim details

--- useful-scripts/install-tools.sh
+++ useful-scripts/install-tools.sh
@@ -1,3 +1,8 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install net-tools git
+sudo apt update
+sudo apt upgrade
+# Install the network tools
+sudo apt install net-tools git
+# Install the git
+sudo apt install git
+# Install vim, vim-tutor and vim-nox
+sudo apt install vim vim-nox
