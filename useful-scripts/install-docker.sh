5e2a3dca390fe0fedb45fe13dd1a6c0549dbcb4c
Subhendu De <sude@microsoft.com>
2022-12-30 22:54:12 +0530

    update the sh files

--- useful-scripts/install-docker.sh
+++ useful-scripts/install-docker.sh
@@ -7,5 +7,6 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo groupadd docker
 sudo usermod -aG docker ${USER}
 groups ${USER}
--- useful-scripts/install-tools.sh
+++ useful-scripts/install-tools.sh
@@ -1,13 +1,28 @@
-sudo apt update
+# Update the repositories and upgrade the packages
+sudo apt update 
 sudo apt upgrade
+
+# REM System Administration tools
 # Install the network tools
 # net-tools - contains tools used in regular work
 # nmap - network mapping tool
-sudo apt install net-tools nmap
-# Install the git
-sudo apt install git
+# ufw - Uncomplicated firewall
+sudo apt install net-tools nmap ufw
 # Install vim, vim-tutor and vim-nox
-sudo apt install vim vim-nox
-# Install other tools
+sudo apt install vim vimtutor vim-nox
+# Install Open SSH server to connect over SSH
+sudo apt install openssh-server
 # vlock - lock the virtual console
 sudo apt install vlock
+
+# REM Developer tools
+# Install the git
+sudo apt install git
+# Install gcc complier
+sudo apt install gcc
+# Install docker
+# Add the docker group, add the user to the docker group and relogin again
+sudo apt install docker.io
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+
